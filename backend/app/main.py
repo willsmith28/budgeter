cd9ebc2acf9974f586a741db31a07deb8161eb4f
will <willsmth0@gmail.com>
2023-12-20 12:47:47 -0500

    use app factory instead of instantiating app directly

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,24 +3,20 @@ from contextlib import asynccontextmanager
 import fastapi
 from psycopg_pool import AsyncConnectionPool
 
-from .env import POSTGRES_CONNINFO
+from app.api import router
+from app.env import POSTGRES_CONNINFO
 
 
 @asynccontextmanager
 async def lifespan(app: fastapi.FastAPI):
-    with AsyncConnectionPool(POSTGRES_CONNINFO, open=False) as pool:
+    """Create and manage connection pool in fastapi lifecycle"""
+    async with AsyncConnectionPool(POSTGRES_CONNINFO, open=False) as pool:
         app.conn_pool = pool
         yield
 
 
 def app_factory():
+    """Create and configure FastAPI instance"""
     app = fastapi.FastAPI(lifespan=lifespan)
+    app.include_router(router)
     return app
-
-
-app = app_factory()
-
-
-@app.get("/")
-def hello_world():
-    return "hello world"
