43f2dc855ad44672b9dd5c11223e96bcd847a4eb
will <willsmth0@gmail.com>
2023-12-23 15:20:16 -0500

    add merchant routes

---
+++ backend/app/routers/api/merchant.py
@@ -0,0 +1,47 @@
+"""Merchant routes routes"""
+import fastapi
+from psycopg import AsyncConnection
+from psycopg.errors import UniqueViolation
+from psycopg.rows import dict_row
+
+from app.dependencies.db import ConnectionPool
+from app.serializers import MerchantIn, MerchantOut
+
+router = fastapi.APIRouter(prefix="/merchant")
+
+
+@router.get("/")
+async def get_all_merchants(connection_pool: ConnectionPool) -> list[MerchantOut]:
+    """get all merchants"""
+    sql = "SELECT * FROM merchant"
+    conn: AsyncConnection
+    async with connection_pool.connection() as conn:
+        async with conn.cursor(row_factory=dict_row) as cursor:
+            await cursor.execute(f"{sql};")
+            return await cursor.fetchall()
+
+
+@router.post("/")
+async def create_merchant(
+    connection_pool: ConnectionPool, merchant: MerchantIn
+) -> MerchantOut:
+    """Create new merchant"""
+    sql = "INSERT INTO merchant (name) VALUES (%(name)s) RETURNING id;"
+    merchant_dict = merchant.model_dump()
+    conn: AsyncConnection
+    try:
+        async with connection_pool.connection() as conn:
+            async with conn.cursor() as cursor:
+                await cursor.execute(sql, merchant_dict)
+                results = await cursor.fetchone()
+                merchant_dict["id"] = results[0]
+
+    except UniqueViolation as err:
+        raise fastapi.HTTPException(
+            status_code=fastapi.status.HTTP_409_CONFLICT,
+            detail={
+                "message": str(err),
+            },
+        )
+
+    return merchant_dict
